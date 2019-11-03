4a7aeac3c72914b6dda1c50bb54901e53bf7f0ff
krisheinrich <krisheinrich@gmail.com>
2019-11-02 17:34:27 -0700

    Add generic Modal component and components for login/registration

---
+++ src/components/LoginModal.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+  
+  const onSubmit = () => {
+    alert("Log in");
+  };
+</script>
+
+<h2>Log In</h2>
+<div>
+  <form on:submit|preventDefault={onSubmit}>
+    <input type="email" id="email" placeholder="Email address"/>
+    <input type="password" id="password" placeholder="Password"/>
+    <button class="button">Log In</button> 
+  </form>
+</div>
+<p>Don't have an account yet? <a href="javascript:;" on:click={() => dispatch('showSignup')}>Sign Up</a></p>
---
+++ src/components/RegistrationModal.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  import axios from 'axios';
+
+  const dispatch = createEventDispatcher();
+  
+  let email = '';
+  let password = '';
+  let passwordConfirmation = '';
+  
+  const onSubmit = async () => {
+    const response = await axios.post('auth/register', { email, password, passwordConfirmation });
+    console.log(response);
+  };
+</script>
+
+<h2>Sign Up</h2>
+<div>
+  <form on:submit|preventDefault={onSubmit}>
+    <input type="email" bind:value={email} placeholder="Email address"/>
+    <input type="password" bind:value={password} placeholder="Password"/>
+    <input type="password" bind:value={passwordConfirmation} placeholder="Enter password again"/>
+    <button class="button">Sign Up</button> 
+  </form>
+</div>
+<p>Already have an account? <a href="javascript:;" on:click={() => dispatch('showLogin')}>Log In</a></p>
