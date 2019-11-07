5f4893634c80890571529685d52b79c87a2e9452
krisheinrich <krisheinrich@gmail.com>
2019-11-07 13:54:13 -0800

    Register user on successful POST; handle unique email error; set default error message

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,5 +1,22 @@
-export const post = (req, res) => {
-  console.log("Body :", JSON.stringify(req.body));
+import { User } from '../../model.js';
 
-  res.end('POST request received correctly');
+export const post = async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  if (password !== passwordConfirmation) {
+    res.end(JSON.stringify({ status: 'error', message: 'Passwords do not match' }));
+    return;
+  }
+
+  try {
+    const user = await User.create({ email, password });
+    res.end(JSON.stringify({ status: 'success', message: 'User added' }));
+  } catch (error) {
+    res.statusCode = 500;
+    let message = 'An error occurred';
+    if (error.name === "SequelizeUniqueConstraintError") {
+      message = 'User already exists';
+    }
+    res.end(JSON.stringify({ status: 'error', message }));
+  }
 };
