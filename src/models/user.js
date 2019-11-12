8536a6569af121e679de2e5fd9b34114f94a4e3e
krisheinrich <krisheinrich@gmail.com>
2019-11-12 13:50:50 -0800

    Add sequelize models for House, Review, and User; add first and last name fields to User model

---
+++ src/models/user.js
@@ -0,0 +1,42 @@
+import { Model, DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt';
+
+import { sequelize } from '../database';
+
+class User extends Model {}
+
+User.init({
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: 'user',
+  timestamps: false,
+  hooks: {
+    beforeCreate: async user => {
+      const saltRounds = 10;
+      const salt = await bcrypt.genSalt(saltRounds);
+      user.password = await bcrypt.hash(user.password, salt);
+    }
+  }
+});
+
+User.prototype.isPasswordValid = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default User;
