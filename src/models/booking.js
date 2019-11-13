34e76991e8d92e67f8f79cd1ff4049c79f4ce892
krisheinrich <krisheinrich@gmail.com>
2019-11-12 18:13:21 -0800

    Use DATEONLY data type for booking date (remove notion of timezone for simplicity)

--- src/models/booking.js
+++ src/models/booking.js
@@ -12,8 +12,8 @@ Booking.init({
   },
   houseId: { type: DataTypes.INTEGER, allowNull: false },
   userId: { type: DataTypes.INTEGER, allowNull: false },
-  startDate: { type: DataTypes.DATE, allowNull: false },
-  endDate: { type: DataTypes.DATE, allowNull: false }
+  startDate: { type: DataTypes.DATEONLY, allowNull: false },
+  endDate: { type: DataTypes.DATEONLY, allowNull: false }
 }, {
   sequelize,
   modelName: 'booking',
